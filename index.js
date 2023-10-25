a43da1982aa2e4f0e0c0b1c62bccce68ef818ca6
monzurul002 <monzurul002@gmail.com>
2023-10-26 00:39:23 +0600

    recipies end point

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 app.use(cors())
 
 const chefs = require('./chef.json')
-
+const recipes = require('./recipes.json')
 app.get("/", (req, res) => {
     res.send("Welcome to karma")
 })
@@ -16,11 +16,19 @@ app.get('/chefs', (req, res) => {
 
 app.get('/chef/:id', (req, res) => {
     const id = parseInt(req.params.id);
-    console.log(id);
+
     const chef = chefs.find(chef => parseInt(chef.id) === id);
     res.send(chef);
 
 });
+
+//category id search
+
+app.get('/recipes/:category_id', (req, res) => {
+    const id = req.params.category_id;
+    const recipes = recipes.filter(recipe => recipe.category_id === id);
+    res.send(recipes)
+})
 
 
 app.listen(port, () => {
