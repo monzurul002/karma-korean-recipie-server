18123282a04affb7f271cfe704ff522b4ce55ab3
monzurul002 <monzurul002@gmail.com>
2023-10-25 23:11:08 +0600

    single chef

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const app = express();
 const port = 5000;
 const cors = require('cors')
 app.use(cors())
+
+const chefs = require('./chef.json')
+
 app.get("/", (req, res) => {
     res.send("Welcome to karma")
 })
 
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id);
+    const chef = chefs.find(chef => parseInt(chef.id) === id);
+    res.send(chef);
+
+});
 
 
 app.listen(port, () => {
